6cb58f142598e54a439eebf9ad359ea66caca31a
gatescn <gatescn@vcu.edu>
2019-08-22 13:36:18 -0400

    Initial commit

---
+++ Main.js
@@ -0,0 +1,10 @@
+import React from 'react';
+import Navbar from './Navbar';
+
+
+export default ({}) => (
+  <div>
+    <Navbar/>
+    <h1>Test component setup</h1>
+  </div>
+  );
---
+++ README.md
@@ -0,0 +1,3 @@
+# react-hcclea
+
+[Edit on StackBlitz ⚡️](https://stackblitz.com/edit/react-hcclea)
